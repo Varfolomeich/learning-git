1a09daab98a9b5ff3f43335d2a34ca4f4788c74c
Varfolomeich <258135222+Varfolomeich@users.noreply.github.com>
2026-01-30 11:57:01 +0700

    Добавил вычитание

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
-const add = (a,b)=>a+b;
-console.log("Сумма чисел 5 и 3: ", add(5,3));
+const add = (a, b) => a + b;
+const sub = (a, b) => a - b; 
+
+console.log("Сумма:", add(5, 3));
+console.log("Разность:", sub(10, 4)); 
